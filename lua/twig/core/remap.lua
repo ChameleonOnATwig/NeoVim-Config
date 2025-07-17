48c008642ad293ad7e3d449f821b7ebb45932148
Maximilian Shah <ChameleonOnATwig@gmail.com>
2025-07-16 23:26:00 -0400

    Added movement and clipboard based remaps. Fixes #2.

--- lua/twig/core/remap.lua
+++ lua/twig/core/remap.lua
@@ -1,4 +1,33 @@
 vim.g.mapleader = " "
+
+-- File Explorer
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
--- Add More Remaps
+-- Moves Selected Text Up or Down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Join Current Line with Line Below
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Makes sure Cursor is Centered when Scrolling
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Makes sure Cursor is Centered when going through Search Results
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Copy to the System Clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- Delete and Paste without Copying to the Clipboard
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- Sets up a Search and Replace for the Current Word under the Cursor
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Makes the Current File Executable
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
