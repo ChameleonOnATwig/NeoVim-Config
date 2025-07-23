8bcbced8e4d5b8740718153d6879337bd82f5ed5
Maximilian Shah <ChameleonOnATwig@gmail.com>
2025-07-23 02:41:26 -0400

    Added a rename function for the LSP

--- lua/twig/plugins/lsp.lua
+++ lua/twig/plugins/lsp.lua
@@ -53,7 +53,8 @@ return {
 			}
 		})
 
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 		vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
 	end
 }
